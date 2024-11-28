95ffd9e201bbaaf99f76707445911a7b61d63244
sakamoto7777 <ssakamoto7657@gmail.com>
2024-11-29 01:15:23 +0900

    認証機能

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -9,14 +9,14 @@ use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        $admin = new Admin();
-         $admin->email = 'admin@example.com';
-         $admin->password = Hash::make('nagoyameshi');
-         $admin->save();
-    }
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    $admin = new Admin();
+    $admin->email = 'admin@example.com';
+    $admin->password = Hash::make('nagoyameshi');
+    $admin->save();
+  }
 }
