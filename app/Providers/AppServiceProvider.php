0d5fa1dc5de4f6b67417b591fea0ef1a8e55fbaa
sakamoto7777 <ssakamoto7657@gmail.com>
2025-06-21 20:03:20 +0900

    メール認証マルチガン

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Pagination\Paginator;
+
 
 class AppServiceProvider extends ServiceProvider
 {
